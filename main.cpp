16e135d21b85a79d36ffde8ac14f41b1a0980864
TatianaIl05 <t.ilinykh1@g.nsu.ru>
2026-04-01 23:57:51 +0700

    add 3 tests for body_contains method

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 ﻿#include <vector>
 #include <functional>
 #include <iostream>
+#include <gtest/gtest.h>
 
 #include "candle.h"
+
+TEST(CandleTest, BodyContainsGreen) {
+  Candle candle{0.0, 3.0, 3.0, 3.0};
+  int price = 5;
+  ASSERT_EQ(candle.body_contains(price), false);
+}
+
+TEST(CandleTest, BodyContainsRed) {
+  Candle candle{5.0, 4.0, 2.0, 3.0};
+  int price = 3;
+  ASSERT_EQ(candle.body_contains(price), true);
+}
+
+TEST(CandleTest, BodyContainsUndefined) {
+  Candle candle{8.0, 1.0, 0.0, 8.0};
+  int price = 8;
+  ASSERT_EQ(candle.body_contains(price), true);
+}
 
 //массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
