ef85254eca572309d8f611c63792488ae063a81b
TatianaIl05 <t.ilinykh1@g.nsu.ru>
2026-04-02 00:03:09 +0700

    add 3 tests for is_green method

--- main.cpp
+++ main.cpp
@@ -86,69 +86,17 @@ TEST(CandleTest, IsRedFalseLimit) {
   ASSERT_EQ(candle.is_red(), false);
 }
 
-//массив всех тестов, который мы заполняем в функции initTests
-static std::vector<std::function<bool()>> tests;
-
-//тест 1
-bool test1()
-{
-  //пример какого-то теста
-  return 42 == (41 + 1); //passed
+TEST(CandleTest, IsGreenTrue) {
+  Candle candle{6.0, 2.0, 5.0, 11.0};
+  ASSERT_EQ(candle.is_green(), true);
 }
 
-//тест 2
-bool test2()
-{
-  //пример какого-то теста
-  return 42 != (41 + 1); //failed
+TEST(CandleTest, IsGreenFalse) {
+  Candle candle{6.0, 2.0, 5.0, 1.0};
+  ASSERT_EQ(candle.is_green(), false);
 }
 
-//тест 3
-bool test3()
-{
-  Candle candle{ 0.0, 3.0, 3.0, 3.0 };
-
-  //пример какого-то теста
-  return candle.high == 3.0;
-}
-
-void initTests()
-{
-  tests.push_back(test1);
-  tests.push_back(test2);
-  tests.push_back(test3);
-  //tests.push_back(test4);
-  //tests.push_back(test5);
-}
-
-int launchTests()
-{
-  int total = 0;
-  int passed = 0;
-
-  for (const auto& test : tests)
-  {
-    std::cout << "test #" << (total + 1);
-    if (test())
-    {
-      passed += 1;
-      std::cout << " passed\n";
-    }
-    else
-    {
-      std::cout << " failed\n";
-    }
-    total += 1;
-  }
-
-  std::cout << "\ntests " << passed << "/" << total << " passed!" << std::endl;
-
-  //0 = success
-  return total - passed;
-}
-
-int main()
-{
-  initTests();
-  return launchTests();
+TEST(CandleTest, IsGreenFalseLimit) {
+  Candle candle{6.0, 2.0, 5.0, 6.0};
+  ASSERT_EQ(candle.is_green(), false);
 }
