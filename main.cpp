a1319ff07f37135bb39a467586a90cda7d9988bf
TatianaIl05 <t.ilinykh1@g.nsu.ru>
2026-04-02 00:02:23 +0700

    add 3 tests for is_red method

--- main.cpp
+++ main.cpp
@@ -71,6 +71,21 @@ TEST(CandleTest, BodySizeZero) {
   ASSERT_EQ(candle.body_size(), 0);
 }
 
+TEST(CandleTest, IsRedTrue) {
+  Candle candle{19.0, 2.0, 5.0, 6.0};
+  ASSERT_EQ(candle.is_red(), true);
+}
+
+TEST(CandleTest, IsRedFalse) {
+  Candle candle{1.0, 2.0, 5.0, 6.0};
+  ASSERT_EQ(candle.is_red(), false);
+}
+
+TEST(CandleTest, IsRedFalseLimit) {
+  Candle candle{6.0, 2.0, 5.0, 6.0};
+  ASSERT_EQ(candle.is_red(), false);
+}
+
 //массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
 
