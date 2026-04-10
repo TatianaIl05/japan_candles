6419f775d658c619f24c64f12df2a361849cbdee
TatianaIl05 <t.ilinykh1@g.nsu.ru>
2026-04-10 15:54:21 +0700

    add k8s/ to .dockerignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 > /usr/share/nginx/html/index.html
 
-if command -v kubectl &> /dev/null && kubectl get pods -l app=repo-app &> /dev/null; then
-    POD_NAME=$(kubectl get pods -l app=repo-app --field-selector status.phase=Running -o name | head -n 1)
+if command -v kubectl &> /dev/null && kubectl get pods -l app=test-repo-app &> /dev/null; then
+    POD_NAME=$(kubectl get pods -l app=test-repo-app --field-selector status.phase=Running -o name | head -n 1)
     
     if [ -n "$POD_NAME" ]; then
         kubectl logs "$POD_NAME" --tail=-1 2>&1 | sed 's/$/<br>/' > /usr/share/nginx/html/index.html
@@ -11,8 +11,8 @@ if command -v kubectl &> /dev/null && kubectl get pods -l app=repo-app &> /dev/n
         nginx -g "daemon off;"
         exit
     fi
-elif docker ps &> /dev/null && docker logs repo-app &> /dev/null; then
-    docker logs repo-app 2>&1 | sed 's/$/<br>/' > /usr/share/nginx/html/index.html
+elif docker ps &> /dev/null && docker logs test-repo-app &> /dev/null; then
+    docker logs test-repo-app 2>&1 | sed 's/$/<br>/' > /usr/share/nginx/html/index.html
 fi
 
 nginx -g "daemon off;"
