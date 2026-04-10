c44bb133e4ba7b4a7cbb18ea21c5c642e7bb4fe1
TatianaIl05 <t.ilinykh1@g.nsu.ru>
2026-04-10 15:38:33 +0700

    fix Dockerfile.web, entrypoint.sh for manifests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-docker logs test-repo-app 2>&1 | sed 's/$/<br>/' > /usr/share/nginx/html/index.html
+
+> /usr/share/nginx/html/index.html
+
+if command -v kubectl &> /dev/null && kubectl get pods -l app=repo-app &> /dev/null; then
+    POD_NAME=$(kubectl get pods -l app=repo-app --field-selector status.phase=Running -o name | head -n 1)
+    
+    if [ -n "$POD_NAME" ]; then
+        kubectl logs "$POD_NAME" --tail=-1 2>&1 | sed 's/$/<br>/' > /usr/share/nginx/html/index.html
+        # Если нашли и записали логи K8s, выходим из скрипта и запускаем nginx
+        nginx -g "daemon off;"
+        exit
+    fi
+elif docker ps &> /dev/null && docker logs repo-app &> /dev/null; then
+    docker logs repo-app 2>&1 | sed 's/$/<br>/' > /usr/share/nginx/html/index.html
+fi
+
 nginx -g "daemon off;"
